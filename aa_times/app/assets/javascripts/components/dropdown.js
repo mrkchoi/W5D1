d6e798b7788cf90892cd996b9f37fdab110dbfe4
mrkchoi <kennethichoi@gmail.com>
2019-06-10 18:03:17 -0700

    Complete dropdown

--- aa_times/app/assets/javascripts/components/dropdown.js
+++ aa_times/app/assets/javascripts/components/dropdown.js
@@ -4,14 +4,14 @@
 // adding hideDropdown listener to document and cleaning up out-of-date listener
 const revealDropdown = (event) => {
   event.stopPropagation(); // prevent event from being picked up by body
-	$('#gear-dropdown').removeClass('hidden');
+	$('.gear-dropdown').removeClass('hidden');
   $('#gear-dropdown-btn').off('click', revealDropdown);
   $(document).on('click', hideDropdown);
 };
 
 // add "hidden" class to dropdown and update event listeners
 const hideDropdown = () => {
-	$('#gear-dropdown').addClass('hidden');
+	$('.gear-dropdown').addClass('hidden');
   $('#gear-dropdown-btn').on('click', revealDropdown);
   $(document).off('click', hideDropdown);
 };
